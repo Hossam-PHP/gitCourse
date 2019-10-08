d558c3147f52e181a457a2d28ed6f86553f63c16
Hossam Rashwan <hossamahmed5487@gmail.com>
2019-10-08 21:21:45 +0200

    Edit Controllers 3

--- controllers/Control1.php
+++ controllers/Control1.php
@@ -1,3 +1,13 @@
 <?php 
 
 echo 'Hello this is first controller';
+
+echo 'Hello this is 55 controller';
+
+echo 'Hello this is 22 controller';
+
+echo 'Hello this is 44 controller';
+
+echo 'Hello this is 88 controller';
+
+echo 'Hello this is 99 controller';
